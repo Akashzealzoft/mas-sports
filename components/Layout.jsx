1b838e4e32158e1c0ac03a8ae77c080fcb2f425d
ariefzealzoft <arief@zealzoft.com>
2024-01-26 16:14:51 +0530

    title change

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>MAS Sports</title>
       </Head>
       <header>
         <Navbar />
